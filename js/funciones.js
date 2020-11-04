d8ab13985e0700715ccd0a6948cd234a6752cd6e
SergioZ <serzupo@gmail.com>
2020-11-04 18:35:52 +0100

    tienda de fruta 3

--- js/funciones.js
+++ js/funciones.js
@@ -6,168 +6,176 @@ var conservaFruta = new Array(false, true, true, true, true, false, true, false,
 var nombrefruta = new Array("Platano", "Manzana", "Pera", "Sandia", "Melon", "Kiwi", "Naranja", "Pomelo", "Melocoton", "Cereza");
 
 
-var almacenObjeto=new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+var almacenObjeto = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 var cantidadFruta = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 var precioTotal = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 //Clases requeridas para la fruta, la cantidad y el precio;
-class Fruta{
-    constructor(nombre,precio){
-        this.nombre=nombre;
-        this.precio=precio;
-        this.cantidad=0;
+class Fruta {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = 0;
     }
 }
 
-class FrutaVerano extends Fruta{
-    constructor(nombre,precio,proximidad,region){
-           super(nombre,precio);
-           this.proximidad=proximidad;
-           this.region=region;
+class FrutaVerano extends Fruta {
+    constructor(nombre, precio, proximidad, region) {
+        super(nombre, precio);
+        this.proximidad = proximidad;
+        this.region = region;
     }
 }
 
-class FrutaInvierno extends Fruta{
-    constructor(nombre,precio,conserva){
-        super(nombre,precio);
-        this.conserva=conserva;
- }
+class FrutaInvierno extends Fruta {
+    constructor(nombre, precio, conserva) {
+        super(nombre, precio);
+        this.conserva = conserva;
+    }
 }
 
 
 
 //variables auxiliares
-var inputs=document.getElementsByTagName("input");
+var inputs = document.getElementsByTagName("input");
 
 var total = 0;
-var aux=0;
+var aux = 0;
 var divEscribir;
 var divLateral;
 var colores;
-
+var permitir = false;
 
 //Ejercicio
 
+//funcion que me permite saber que imagen ha sido pulsada y gracias a la posicion, creo el objeto con los datos necesarios
 function almacen(num) {
     for (var i = 0; i < 10; i++) {
         if (i == num) {
-            if(almacenObjeto[i]==0 && i%2==0){
-                let frutaV=new FrutaVerano();
-                frutaV.nombre=nombrefruta[i];
-                frutaV.precio=precioFruta[i];
-                frutaV.proximidad=proximidadFruta[i];
-                frutaV.region=regionFruta[i];
-                almacenObjeto[i]=frutaV;
+            if (almacenObjeto[i] == 0 && i % 2 == 0) { //si es par es de un tipo y si no de otro
+                let frutaV = new FrutaVerano();
+                frutaV.nombre = nombrefruta[i];
+                frutaV.precio = precioFruta[i];
+                frutaV.proximidad = proximidadFruta[i];
+                frutaV.region = regionFruta[i];
+                almacenObjeto[i] = frutaV;
 
-            }else if(almacenObjeto[i]==0 && i%2!=0){
-                let frutaI=new FrutaInvierno();
-                frutaI.nombre=nombrefruta[i];
-                frutaI.precio=precioFruta[i];
-                almacenObjeto[i]=frutaI;
+            } else if (almacenObjeto[i] == 0 && i % 2 != 0) {
+                let frutaI = new FrutaInvierno();
+                frutaI.nombre = nombrefruta[i];
+                frutaI.precio = precioFruta[i];
+                almacenObjeto[i] = frutaI;
 
             }
-            cantidad(num);
+            cantidad(num); //metodo que verifica si los kilos estan bien añadidos, si lo estan añado los kilos al objeto
             precioTotal[i] = almacenObjeto[i].cantidad * almacenObjeto[i].precio;
         }
     }
 
 }
 
-function cantidad(num){
-    if(inputs[num].value==""){
-        alert("No has introducido un numero")
-    }else{
-        almacenObjeto[num].cantidad+=Number(inputs[num].value);
+function cantidad(num) {
+    if (inputs[num].value == "" || Number(inputs[num].value) < 1 || Number.isInteger(Number(inputs[num].value)) == false) {
+        alert("No has introducido un numero entero")
+    } else {
+        permitir = true;
+        almacenObjeto[num].cantidad += Number(inputs[num].value);
         lateral(num, almacenObjeto[num].nombre);
-        inputs[num].value="";
+        inputs[num].value = "";
     }
-    
+
 }
 
-function lateral(num,nombre){
-    divLateral=document.getElementById("lateral");
-    divLateral.innerHTML += "<p class='fruta' name='"+almacenObjeto[num].nombre+"'> Nombre: " + almacenObjeto[num].nombre + " ---  Kilos"+ " --- "+almacenObjeto[num].cantidad+"</p>";
-    colores=document.getElementsByClassName('fruta');
+//funcion que me insertara en el div lateral toda la informacion necesaria de la fruta antes pulsada, ademas le cambio el color de fondo como el ejercicio pide
+function lateral(num, nombre) {
+    divLateral = document.getElementById("lateral");
+    divLateral.innerHTML += "<p class='fruta' name='" + almacenObjeto[num].nombre + "'> Nombre: " + almacenObjeto[num].nombre + " ---  Kilos" + " --- " + almacenObjeto[num].cantidad + "</p>";
+    colores = document.getElementsByClassName('fruta');
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
     var bgColor = "rgb(" + x + "," + y + "," + z + ")";
 
-   for (let index = 0; index < colores.length; index++) {
-       if(colores[index].getAttribute("name")==nombre){
-        colores[index].style.backgroundColor =bgColor;
-       }
-       if(colores[index].getAttribute("name")!=nombre){
-        colores[index].removeAttribute("style");
-       }
-       
-   }
+    for (let index = 0; index < colores.length; index++) {
+        if (colores[index].getAttribute("name") == nombre) {
+            colores[index].style.backgroundColor = bgColor;
+        }
+        if (colores[index].getAttribute("name") != nombre) {
+            colores[index].removeAttribute("style");
+        }
+
+    }
 }
 
 
-
+//funcion que me activa el boton cuando el cliente ha acabado de comprar,muestra todos los valores en el div de abajo, crea una ventana externa con datos de la fruta y genera el timer para reiniciar todo
 function enviar() {
-    divEscribir = document.getElementById("final");
-    divEscribir.innerHTML = "";
-    divEscribir.innerHTML +=new Date()
-    for (var i = 0; i < 10; i++) {
-        if (almacenObjeto[i] != 0) {
-            divEscribir.innerHTML += "<p>" + almacenObjeto[i].nombre + " --- " + almacenObjeto[i].cantidad + " Kilo"+ " --- "+almacenObjeto[i].precio.toFixed(2)+ "€ "+ " --- "+(almacenObjeto[i].cantidad*almacenObjeto[i].precio).toFixed(2)+ "€ </p>";
-            total += precioTotal[i]
-            aux+=almacenObjeto[i].cantidad;
+
+    if (permitir) {
+        divEscribir = document.getElementById("final");
+        divEscribir.innerHTML = "";
+        divEscribir.innerHTML += new Date()
+        for (var i = 0; i < 10; i++) {
+            if (almacenObjeto[i] != 0) {
+                divEscribir.innerHTML += "<p>" + almacenObjeto[i].nombre + " --- " + almacenObjeto[i].cantidad + " Kilo" + " --- " + almacenObjeto[i].precio.toFixed(2) + "€ " + " --- " + (almacenObjeto[i].cantidad * almacenObjeto[i].precio).toFixed(2) + "€ </p>";
+                total += precioTotal[i]
+                aux += almacenObjeto[i].cantidad;
+            }
+            inputs[i].value = "";
+
         }
-        inputs[i].value="";
-       
+        divEscribir.innerHTML += "<p> Precio Total: " + total.toFixed(2) + " Kilo</p>";
+        divEscribir.innerHTML += "<p> Precio Medio:" + (total / aux).toFixed(3) + "€</p>";
+
+        total = 0;
+        aux = 0;
+
+        var myWindow = window.open("", "Resultado", "width=400,height=300");
+        myWindow.document.write(ventana());
+
+        setTimeout("reiniciar()", 10000);
     }
-    divEscribir.innerHTML += "<p> Precio Total: " + total.toFixed(2) + " Kilo</p>";
-    divEscribir.innerHTML += "<p> Precio Medio:" + (total / aux).toFixed(3) + "€</p>";
+
+
+}
+
+function ventana() {
+    var texto = "";
+    for (var i = 0; i < 10; i++) {
+        if (almacenObjeto[i] != 0 && i % 2 == 0) {
+            if (almacenObjeto[i].proximidad == true) {
+                texto += " Las " + almacenObjeto[i].nombre + " son fruta de verano, de proximidad y de " + almacenObjeto[i].region + "\n";
+            } else {
+                texto += " Las " + almacenObjeto[i].nombre + " son fruta de verano,no de proximidad y de " + almacenObjeto[i].region + "\n";
+            }
+        } else if (almacenObjeto[i] != 0 && i % 2 != 0) {
+            if (almacenObjeto[i].conserva == true) {
+                texto += " Las " + almacenObjeto[i].nombre + "son frutas de inverno y es recomendable conservarlas en fuera de la nevera " + "\n"
+            } else {
+                texto += " Las " + almacenObjeto[i].nombre + "son frutas de inverno y no es recomendable conservarlas en fuera de la nevera " + "\n"
+            }
+
+        }
+    }
+    return texto;
+
+}
+
+
+function reiniciar() {
+    divEscribir.innerHTML = '';
+    divLateral.innerHTML = "";
+
+    for (var i = 0; i < 10; i++) {
+        almacenObjeto[i] = 0
+        cantidadFruta[i] = 0
+        precioTotal[i] = 0
+    }
+    inputs = document.getElementsByTagName("input");
 
     total = 0;
     aux = 0;
-
-    ventana()
-
-    setTimeout("reiniciar()",10000);
-    
-}
-
-function ventana(){
-    var texto="";
-    for (var i = 0; i < 10; i++) {
-        if ( almacenObjeto[i]!=0 && i%2==0) {
-            if(almacenObjeto[i].proximidad==true){
-               texto+=  " Las "+almacenObjeto[i].nombre+" son fruta de verano, de proximidad y de "+almacenObjeto[i].region+"\n";
-            }else{
-                texto+= " Las "+almacenObjeto[i].nombre+" son fruta de verano,no de proximidad y de "+almacenObjeto[i].region+"\n";
-            }
-        }else if(almacenObjeto[i]!=0 && i%2!=0){
-            if(almacenObjeto[i].conserva==true){
-                texto+= " Las "+almacenObjeto[i].nombre+ "son frutas de inverno y es recomendable conservarlas en fuera de la nevera "+"\n"
-           }else{
-            texto+= " Las "+almacenObjeto[i].nombre+ "son frutas de inverno y no es recomendable conservarlas en fuera de la nevera "+"\n"
-           }
-            
-        }
-    }
-    alert(texto);
-    
-}
-
-
-function reiniciar(){
-    divEscribir.innerHTML = '';
-    divLateral.innerHTML="";
-
-    for (var i = 0; i < 10; i++) {
-        almacenObjeto[i]=0
-        cantidadFruta[i]=0
-        precioTotal[i]=0
-    }
-     inputs=document.getElementsByTagName("input");
-
-   total = 0;
-   aux=0;
-   divEscribir="";
-   divLateral="";
-   colores="";
+    divEscribir = "";
+    divLateral = "";
+    colores = "";
 }
